c64db258919c453f4deab7ff14c13a43d92b5c6b
PikminGuts92 <PikminGuts92@gmail.com>
2016-09-30 15:50:21 -0400

    Implemented navigation menu for Bridge search in main.

--- TwilightBridge/Program.cs
+++ TwilightBridge/Program.cs
@@ -10,21 +10,132 @@ namespace TwilightBridge
     {
         static void Main(string[] args)
         {
-            Bridge sim = new Bridge(4);
-            //sim.RunLongTest();
-            //sim.Run();
-            sim.IterativeRun(Search.UniformCost);
-            Console.ReadKey();
+            Console.WriteLine("Twilight Bridge v1.0 by Francisco Martinez");
+            Bridge sim = new Bridge();
+
+            // Input char
+            char c;
+
+            do
+            {
+                // Displays output to user
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                Console.WriteLine("\t\'a\' - Set people");
+                Console.WriteLine("\t\'b\' - Set search type");
+                Console.WriteLine("\t\'r\' - Run");
+                Console.WriteLine("\t\'x\' - Exit");
+                Console.WriteLine();
+
+                // Reads input
+                Console.Write("Enter option: ");
+                string input = Console.ReadLine();
+
+                // Converts string to char
+                if (!Char.TryParse(input, out c))
+                    c = '0';
+
+                switch(c)
+                {
+                    case 'a':
+                    case 'A':
+                        int count = InputCount();
+                        int[] costs = new int[count];
+
+                        for (int i = 0; i < costs.Length; i++)
+                        {
+                            costs[i] = InputCost(i);
+                        }
+
+                        // Sets costs
+                        sim.SetCosts(costs);
+
+                        break;
+                    case 'b':
+                    case 'B':
+                        // Sets search type
+                        InputSearch(sim);
+                        break;
+                    case 'c':
+                    case 'C':
+                        break;
+                    case 'r':
+                    case 'R':
+                        sim.IterativeRun();
+                        break;
+                }
+
+
+            } while (c != 'x' && c != 'X');
         }
 
         static int InputCount()
         {
-            return 3;
+            int count;
+
+            do
+            {
+                // Gets count input from user
+                Console.Write("Enter number of people: ");
+                string input = Console.ReadLine();
+
+                // Converts string to int
+                if (!Int32.TryParse(input, out count))
+                    count = 0;
+
+            } while (count < 1);
+
+            return count;
         }
 
-        static int InputCost()
+        static int InputCost(int idx)
         {
-            return 0;
+            int cost;
+
+            do
+            {
+                // Gets cost input from user
+                Console.Write("Enter cost for person {0}: ", idx + 1);
+                string input = Console.ReadLine();
+
+                // Converts string to int
+                if (!Int32.TryParse(input, out cost))
+                    cost = -1;
+
+            } while (cost < 0);
+
+            return cost;
+        }
+
+        static void InputSearch(Bridge sim)
+        {
+            int searchNum;
+
+            do
+            {
+                // Gets search input from user
+                Console.Write("Enter search type: ");
+                string input = Console.ReadLine();
+
+                // Converts string to int
+                if (!Int32.TryParse(input, out searchNum))
+                    searchNum = -1;
+
+                switch(searchNum)
+                {
+                    case 0:
+                        sim.SetSearch(Search.BreadthFirst);
+                        break;
+                    case 1:
+                        sim.SetSearch(Search.DepthFirst);
+                        break;
+                    case 2:
+                        sim.SetSearch(Search.UniformCost);
+                        break;
+                }
+
+            } while (searchNum < 0 || searchNum > 2);
+
         }
     }
 }
