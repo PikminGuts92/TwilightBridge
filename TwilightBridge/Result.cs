bb50eedbb43dc433f953b2cd9873053201ba87cb
PikminGuts92 <PikminGuts92@gmail.com>
2016-10-02 12:18:50 -0400

    Added more stats to Result class.

--- TwilightBridge/Result.cs
+++ TwilightBridge/Result.cs
@@ -11,16 +11,22 @@ namespace TwilightBridge
         private readonly State _winState;
         private readonly Search _searchType;
         private readonly long _computeTime; // In milliseconds
+        private readonly int _numPeople;
+        private readonly long _fringeItr;
 
-        public Result(State winState, Search searchType, long computeTime)
+        public Result(State winState, Search searchType, long computeTime, int numPeople, long fringeItr)
         {
             _winState = winState;
             _searchType = searchType;
             _computeTime = computeTime;
+            _numPeople = numPeople;
+            _fringeItr = fringeItr;
         }
 
         public State WinPath { get { return _winState; } }
         public Search SearchType { get { return _searchType; } }
         public long ComputeTime { get { return _computeTime; } }
+        public int NumberOfPeople { get { return _numPeople; } }
+        public long FringeIterations { get { return _fringeItr; } }
     }
 }
