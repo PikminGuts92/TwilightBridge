78cc93b48aec29c6dffe72b0b6adc0875e9d83d9
PikminGuts92 <PikminGuts92@gmail.com>
2016-10-02 11:30:50 -0400

    Added Result class.

---
+++ TwilightBridge/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwilightBridge
+{
+    public class Result
+    {
+        private readonly State _winState;
+        private readonly Search _searchType;
+        private readonly int _computeTime; // In milliseconds
+
+        public Result(State winState, Search searchType, int computeTime)
+        {
+            _winState = winState;
+            _searchType = searchType;
+            _computeTime = computeTime;
+        }
+
+        public State WinPath { get { return _winState; } }
+        public Search SearchType { get { return _searchType; } }
+        public int ComputeTime { get { return _computeTime; } }
+    }
+}
