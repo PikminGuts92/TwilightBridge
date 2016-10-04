d267c588fe18659c3d91bda7b71210476ec7876c
PikminGuts92 <PikminGuts92@gmail.com>
2016-10-04 17:45:02 -0400

    Added comments.

--- TwilightBridge/Result.cs
+++ TwilightBridge/Result.cs
@@ -15,6 +15,15 @@ namespace TwilightBridge
         private readonly int _numPeople;
         private readonly long _fringeItr;
 
+        /// <summary>
+        /// Constructor for Result object
+        /// </summary>
+        /// <param name="winState">Win path</param>
+        /// <param name="searchType">Search ysed</param>
+        /// <param name="computeTime">Compute time (ms)</param>
+        /// <param name="computeTimeTicks">Compute time (ticks)</param>
+        /// <param name="numPeople">Number of people</param>
+        /// <param name="fringeItr">Number of fringe iterations</param>
         public Result(State winState, Search searchType, long computeTime, long computeTimeTicks, int numPeople, long fringeItr)
         {
             _winState = winState;
@@ -25,11 +34,34 @@ namespace TwilightBridge
             _fringeItr = fringeItr;
         }
 
+        /// <summary>
+        /// Gets win path
+        /// </summary>
         public State WinPath { get { return _winState; } }
+
+        /// <summary>
+        /// Gets search type
+        /// </summary>
         public Search SearchType { get { return _searchType; } }
+
+        /// <summary>
+        /// Gets compute time (ms)
+        /// </summary>
         public long ComputeTime { get { return _computeTime; } }
+
+        /// <summary>
+        /// Gets compute time (ticks)
+        /// </summary>
         public long ComputeTimeTicks { get { return _computTimeTicks; } }
+
+        /// <summary>
+        /// Gets number of people
+        /// </summary>
         public int NumberOfPeople { get { return _numPeople; } }
+
+        /// <summary>
+        /// Gets number of fringe iterations
+        /// </summary>
         public long FringeIterations { get { return _fringeItr; } }
     }
 }
